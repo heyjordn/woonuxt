82a341d21be2e5b95dd295fdd3635d733c723c2e
Jordan Jones <jordan@osoobe.com>
2023-10-17 13:33:14 -0500

    feat: removed extended theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,80 @@
-export default defineNuxtConfig({
-  // Get all the pages, components, composables and plugins from the parent theme
-  extends: ['./woonuxt_base'],
+import pkg from './package.json';
+import { createResolver } from '@nuxt/kit';
+const { resolve } = createResolver(import.meta.url);
 
-  // Depending on your servers capabilities, you may need to adjust the following settings.
-  // It will affect the build time but also increase the reliability of the build process.
-  // If you have a server with a lot of memory and CPU, you can remove the following settings.
-  nitro: {
-    prerender: {
-      concurrency: 10, // How many pages to prerender at once
-      interval: 1000, // How long to wait between prerendering pages
-      failOnError: false, // This stops the build from failing but the page will not be statically generated
+export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en' },
+      link: [{ rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
     },
+    pageTransition: { name: 'page', mode: 'out-in' },
+  },
+
+  plugins: [resolve('./plugins/init.ts')],
+
+  components: [{ path: resolve('./components'), pathPrefix: false }],
+
+  modules: ['woonuxt-settings', 'nuxt-graphql-client', '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/image', '@nuxtjs/i18n'],
+
+  image: {
+    domains: process.env.NUXT_IMAGE_DOMAINS ? process.env.NUXT_IMAGE_DOMAINS.replace(/ /g, '').split(',') : [],
+  },
+  pages: true,
+
+  hooks: {
+    'pages:extend'(pages) {
+      pages.push({
+        name: 'product-page-pager',
+        path: '/products/page/:pageNumber',
+        file: resolve('./pages/products.vue'),
+      });
+      pages.push({
+        name: 'product-category-page',
+        path: '/product-category/:categorySlug',
+        file: resolve('./pages/products.vue'),
+      });
+      pages.push({
+        name: 'product-category-page-pager',
+        path: '/product-category/:categorySlug/page/:pageNumber',
+        file: resolve('./pages/products.vue'),
+      });
+      pages.push({
+        name: 'order-received',
+        path: '/checkout/order-received/:orderId',
+        file: resolve('./pages/order-summary.vue'),
+      });
+      pages.push({
+        name: 'order-summary',
+        path: '/order-summary/:orderId',
+        file: resolve('./pages/order-summary.vue'),
+      });
+    },
+  },
+
+  routeRules: {
+    '/checkout/order-received/**': { ssr: false },
+    '/order-summary/**': { ssr: false },
+  },
+
+  runtimeConfig: {
+    public: {
+      version: pkg.version || '0.0.0',
+    },
+  },
+
+  // Multilingual support
+  i18n: {
+    locales: [
+      { code: 'en', file: 'en-US.json', name: 'English' },
+      { code: 'de', file: 'de-DE.json', name: 'Deutsch' },
+      { code: 'es', file: 'es-ES.json', name: 'Español' },
+      { code: 'fr', file: 'fr-FR.json', name: 'Français' },
+      { code: 'it', file: 'it-IT.json', name: 'Italiano' },
+      { code: 'pt', file: 'pt-BR.json', name: 'Português' },
+    ],
+    langDir: 'locales',
+    defaultLocale: 'en',
+    strategy: 'no_prefix',
   },
 });
